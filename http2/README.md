00440709a95bcf7fbb2accfab3d0139881c78246
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2023-08-29 07:36:51 +0100

    Add HTTP/2 example

---
+++ http2/README.md
@@ -0,0 +1,5 @@
+# Spring with HTTP/2
+
+See https://brytecode.be/articles/using-http-2-in-your-spring-boot-application/
+
+Note that HTTP/2 only works with HTTPS.
---
+++ http2/src/test/java/com/example/http2/Http2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.http2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Http2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
