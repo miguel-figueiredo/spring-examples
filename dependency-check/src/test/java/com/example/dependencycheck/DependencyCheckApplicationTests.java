459dc6eda244fa34bf984d6c838b498a2a4cb5f5
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2023-09-01 18:18:44 +0100

    Add dependency check example

---
+++ dependency-check/src/test/java/com/example/dependencycheck/DependencyCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dependencycheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
