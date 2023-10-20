3e51c3431f6b7329675c8de99b889ade53ec6f53
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2023-10-20 16:25:55 +0100

    Add parallel tests

---
+++ parallel-tests/src/main/java/com/example/parallel/ParallelTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.parallel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParallelTestsApplication.class, args);
+	}
+
+}
---
+++ parallel-tests/src/test/java/com/example/parallel/IntegrationTest.java
@@ -0,0 +1,13 @@
+package com.example.parallel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
