4dac02a87f8eb1d8964e9061af5a8dcfaf84bd6b
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2023-10-23 15:04:21 +0100

    Add redis to persistent retry

--- persistent-retry/src/main/java/com/example/retry/RedisRetryContextCache.java
+++ persistent-retry/src/main/java/com/example/retry/RedisRetryContextCache.java
@@ -1,27 +1,35 @@
 package com.example.retry;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.retry.RetryContext;
 import org.springframework.retry.policy.RetryCacheCapacityExceededException;
 import org.springframework.retry.policy.RetryContextCache;
 
 public class RedisRetryContextCache implements RetryContextCache {
+
+    @Autowired
+    private RedisTemplate<Object, Object> redisTemplate;
+
     @Override
     public RetryContext get(final Object key) {
-        return null;
+        return (RetryContext) redisTemplate.opsForValue().get(key);
     }
 
     @Override
     public void put(final Object key, final RetryContext context) throws RetryCacheCapacityExceededException {
+        redisTemplate.opsForValue().set(key, context);
 
     }
 
     @Override
     public void remove(final Object key) {
+        redisTemplate.delete(key);
 
     }
 
     @Override
     public boolean containsKey(final Object key) {
-        return false;
+        return redisTemplate.hasKey(key);
     }
 }
