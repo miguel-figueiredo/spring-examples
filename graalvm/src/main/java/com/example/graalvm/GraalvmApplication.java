fb380e97a499cc7a863364c6b26ae8fea62110bb
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2023-11-08 12:14:27 +0000

    Add GraalVM

---
+++ graalvm/src/main/java/com/example/graalvm/GraalvmApplication.java
@@ -0,0 +1,13 @@
+package com.example.graalvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmApplication.class, args);
+	}
+
+}
---
+++ graalvm/src/test/java/com/example/graalvm/GraalvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graalvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
