a9485e90566c0e01ade37432620f0afe79996bf8
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2024-01-15 15:24:05 +0000

    Add JPA collections

---
+++ jpa-collections/src/test/java/com/example/jpa/collections/JpaCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa.collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
