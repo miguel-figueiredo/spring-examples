82d525ca6ea96051bc78bacd36002d20c88bf7a9
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2024-09-20 17:16:46 +0100

    Add redis as backing store

--- debezium/src/main/java/com/example/debezium/DebeziumConfiguration.java
+++ debezium/src/main/java/com/example/debezium/DebeziumConfiguration.java
@@ -4,51 +4,50 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
 import java.io.IOException;
 
 @Configuration
 public class DebeziumConfiguration {
 
-    @Value("${database.host}")
-    private String databaseHost;
+    @Value("${postgres.host}")
+    private String postgresHost;
 
-    @Value("${database.port}")
-    private int databasePort;
+    @Value("${postgres.port}")
+    private int postgresPort;
 
-    @Value("${database.name}")
-    private String databaseName;
+    @Value("${postgres.database}")
+    private String postgresDatabase;
 
-    @Value("${database.user}")
-    private String databaseUser;
+    @Value("${postgres.user}")
+    private String postgresUser;
 
-    @Value("${database.password}")
-    private String databasePassword;
+    @Value("${postgres.password}")
+    private String postgresPassword;
+
+    @Value("${redis.address}")
+    private String redisAddress;
 
     @Bean
     public io.debezium.config.Configuration customerConnector() throws IOException {
-        var offsetStorageTempFile = File.createTempFile("offsets_", ".dat");
         return io.debezium.config.Configuration.create()
-                .with("name", "customer-postres-connector")
+                .with("name", "person-connector")
                 .with("connector.class", "io.debezium.connector.postgresql.PostgresConnector")
-                .with("database.hostname", databaseHost)
-                .with("database.port", databasePort)
-                .with("database.user", databaseUser)
-                .with("database.password", databasePassword)
-                .with("database.dbname", databaseName)
-                .with("table.include.list", "public.person")
-                .with("include.schema.changes", "false")
-                .with("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore")
-                .with("offset.storage.file.filename", offsetStorageTempFile.getAbsolutePath())
-                .with("offset.flush.interval.ms", "60000")
+                .with("database.hostname", postgresHost)
+                .with("database.port", postgresPort)
+                .with("database.user", postgresUser)
+                .with("database.password", postgresPassword)
+                .with("database.dbname", postgresDatabase)
                 .with("database.server.id", "10181")
                 .with("database.server.name", "person-postgres-db-server")
-                .with("database.history", "io.debezium.relational.history.MemoryDatabaseHistory")
+                .with("table.include.list", "public.person")
+                .with("include.schema.changes", "false")
+                .with("offset.storage", "io.debezium.storage.redis.offset.RedisOffsetBackingStore")
+                .with("offset.flush.interval.ms", "60000")
+                .with("offset.storage.redis.address", redisAddress)
                 .with("publication.autocreate.mode", "filtered")
                 .with("plugin.name", "pgoutput")
                 .with("slot.name", "dbz_persondb_listener")
-                .with("topic.prefix", "test")
-
+                .with("topic.prefix", "person-topic")
                 .build();
     }
 }
