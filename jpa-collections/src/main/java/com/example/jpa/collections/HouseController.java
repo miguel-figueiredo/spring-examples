315d28427f6938e87d78df84632be37bd900de17
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2024-11-08 15:40:23 +0000

    Add one to many

--- jpa-collections/src/main/java/com/example/jpa/collections/HouseController.java
+++ jpa-collections/src/main/java/com/example/jpa/collections/HouseController.java
@@ -1,4 +1,33 @@
 package com.example.jpa.collections;
 
+import com.example.jpa.collections.model.Door;
+import com.example.jpa.collections.model.House;
+import com.example.jpa.collections.repository.HouseRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/houses")
 public class HouseController {
+
+    private final HouseRepository houseRepository;
+
+    public HouseController(HouseRepository houseRepository) {
+        this.houseRepository = houseRepository;
+    }
+
+    @PostMapping
+    @Transactional
+    public void createHouse() {
+        final Door door = new Door(UUID.randomUUID());
+        final House house = new House(UUID.randomUUID(), List.of(door));
+        houseRepository.save(house);
+    }
+
 }
--- jpa-collections/src/main/java/com/example/jpa/collections/model/House.java
+++ jpa-collections/src/main/java/com/example/jpa/collections/model/House.java
@@ -2,7 +2,10 @@ package com.example.jpa.collections.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
 import java.util.List;
 import java.util.UUID;
@@ -14,6 +17,10 @@ public class House {
     private UUID id;
 
     @OneToMany
+    // Avoids an association table
+    @JoinColumn(name="house_id")
+    // Allows for persisting doors when persisting the house
+    @Cascade(CascadeType.ALL)
     private List<Door> doors;
 
     public House() {
