b393beb457a7450e44cf8446713e0152ff56d5e4
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2024-12-05 09:42:02 +0000

    Add hello world

---
+++ hello/src/main/java/com/example/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    String hello() {
+        return "Hello, world!";
+    }
+
+}
