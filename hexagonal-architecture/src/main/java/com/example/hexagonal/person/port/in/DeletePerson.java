782041095f1e54da418ce6a780db4bb960be81f7
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2024-12-05 15:27:55 +0000

    Hex arch example

--- hexagonal-architecture/src/main/java/com/example/hexagonal/person/port/in/DeletePerson.java
+++ hexagonal-architecture/src/main/java/com/example/hexagonal/person/port/in/DeletePerson.java
@@ -1,6 +1,10 @@
 package com.example.hexagonal.person.port.in;
 
+import com.example.hexagonal.util.Validation;
+import jakarta.validation.constraints.NotNull;
+
+@Validation
 public interface DeletePerson {
 
-    void execute(Long personId);
+    void execute(@NotNull Long personId);
 }
--- hexagonal-architecture/src/main/java/com/example/hexagonal/person/port/in/UpdatePerson.java
+++ hexagonal-architecture/src/main/java/com/example/hexagonal/person/port/in/UpdatePerson.java
@@ -1,10 +1,10 @@
 package com.example.hexagonal.person.port.in;
 
 import com.example.hexagonal.person.business.model.Person;
+import com.example.hexagonal.util.Validation;
 import jakarta.validation.Valid;
-import org.springframework.validation.annotation.Validated;
 
-@Validated
+@Validation
 public interface UpdatePerson {
 
     void execute(@Valid Person person);
