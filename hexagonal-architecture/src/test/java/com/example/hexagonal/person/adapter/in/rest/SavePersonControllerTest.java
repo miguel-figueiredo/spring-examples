3b38c28cc21d09da3a3dca1e555df2ada6d63ee5
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2024-12-06 10:02:38 +0000

    Hex arch example

--- hexagonal-architecture/src/test/java/com/example/hexagonal/person/adapter/in/rest/SavePersonControllerTest.java
+++ hexagonal-architecture/src/test/java/com/example/hexagonal/person/adapter/in/rest/SavePersonControllerTest.java
@@ -3,15 +3,24 @@ package com.example.hexagonal.person.adapter.in.rest;
 import com.example.hexagonal.person.port.in.SavePerson;
 import com.example.hexagonal.person.port.in.SavePerson.NewPerson;
 import com.example.hexagonal.spring.CustomErrorHandler;
+import com.example.hexagonal.spring.HexagonalArchitectureApplication;
 import io.restassured.http.ContentType;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
@@ -19,9 +28,15 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 
 @ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(
+        classes = {
+                HexagonalArchitectureApplication.class
+        }
+)
 class SavePersonControllerTest {
 
-    @Mock
+    @Autowired
     SavePerson savePerson;
 
     @BeforeEach
@@ -49,13 +64,12 @@ class SavePersonControllerTest {
             .statusCode(400);
     }
 
-    @Test
-    void validationError() {
-        doThrow(new ConstraintViolationException(Set.of())).when(savePerson).execute(any());
-
+    @ParameterizedTest
+    @MethodSource("invalidPersons")
+    void validationError(NewPerson person) {
         given()
             .contentType(ContentType.JSON)
-            .body(new NewPerson("First", "Last"))
+            .body(person)
         .when()
             .post("/api/persons")
         .then()
@@ -71,5 +85,14 @@ class SavePersonControllerTest {
                 .post("/api/persons")
         .then()
             .statusCode(200);
+    }
+
+    public static Stream<Arguments> invalidPersons() {
+        return Stream.of(
+            Arguments.of(new NewPerson(null, "Last")),
+            Arguments.of(new NewPerson("", "Last")),
+            Arguments.of(new NewPerson("First", "")),
+            Arguments.of(new NewPerson("First", null))
+        );
     }
 }
