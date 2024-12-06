d81ec8219a01aaceb88f3d67a9865cab70ebc27b
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2024-12-06 10:18:37 +0000

    Hex arch example

--- hexagonal-architecture/src/test/java/com/example/hexagonal/person/adapter/in/rest/MockBeanConfiguration.java
+++ hexagonal-architecture/src/test/java/com/example/hexagonal/person/adapter/in/rest/MockBeanConfiguration.java
@@ -1,14 +1,15 @@
 package com.example.hexagonal.person.adapter.in.rest;
 
 import com.example.hexagonal.person.port.in.SavePerson;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+
+import static org.mockito.Mockito.mock;
 
 @TestConfiguration
 public class MockBeanConfiguration {
     @Bean
     public SavePerson savePerson() {
-        return Mockito.mock(SavePerson.class);
+        return mock(SavePerson.class);
     }
 }
