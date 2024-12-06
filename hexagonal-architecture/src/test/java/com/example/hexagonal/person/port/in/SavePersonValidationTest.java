76e564d6bdee08e4257896cd38a7a148c63417c6
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2024-12-06 09:48:06 +0000

    Hex arch example

--- hexagonal-architecture/src/test/java/com/example/hexagonal/person/port/in/SavePersonValidationTest.java
+++ hexagonal-architecture/src/test/java/com/example/hexagonal/person/port/in/SavePersonValidationTest.java
@@ -4,17 +4,24 @@ import com.example.hexagonal.person.port.in.SavePerson.NewPerson;
 import com.example.hexagonal.spring.HexagonalArchitectureApplication;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.stream.Stream;
 
-// Required because the main class is not in the root package.
 // BOGUS: makes port or use cases depend on spring, and causes a failure in the Arch Unit tests.
-@SpringBootTest(classes = HexagonalArchitectureApplication.class)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(
+        classes = {
+                HexagonalArchitectureApplication.class
+        }
+)
 public class SavePersonValidationTest {
 
     @Autowired
@@ -28,10 +35,10 @@ public class SavePersonValidationTest {
 
     public static Stream<Arguments> invalidPersons() {
         return Stream.of(
-            Arguments.of(new NewPerson(null, "Last")),
-            Arguments.of(new NewPerson("", "Last")),
-            Arguments.of(new NewPerson("First", "")),
-            Arguments.of(new NewPerson("First", null))
+                Arguments.of(new NewPerson(null, "Last")),
+                Arguments.of(new NewPerson("", "Last")),
+                Arguments.of(new NewPerson("First", "")),
+                Arguments.of(new NewPerson("First", null))
         );
     }
 
