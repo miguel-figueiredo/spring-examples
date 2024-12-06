2ffba816064c692f8cab043498e1416975b45d56
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2024-12-06 09:03:51 +0000

    Hex arch example

---
+++ hexagonal-architecture/src/test/java/com/example/hexagonal/person/port/in/SavePersonValidationTest.java
@@ -0,0 +1,37 @@
+package com.example.hexagonal.person.port.in;
+
+import com.example.hexagonal.person.port.in.SavePerson.NewPerson;
+import com.example.hexagonal.spring.HexagonalArchitectureApplication;
+import jakarta.validation.ConstraintViolationException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.Stream;
+
+// Required because the main class is not in the root package.
+@SpringBootTest(classes = HexagonalArchitectureApplication.class)
+public class SavePersonValidationTest {
+
+    @Autowired
+    SavePerson savePerson;
+
+    @ParameterizedTest
+    @MethodSource("invalidPersons")
+    void shouldThrowExceptionForInvalidPersons(NewPerson person) {
+        Assertions.assertThrows(ConstraintViolationException.class, () -> savePerson.execute(person));
+    }
+
+    public static Stream<Arguments> invalidPersons() {
+        return Stream.of(
+            Arguments.of(new NewPerson(null, "Last")),
+            Arguments.of(new NewPerson("", "Last")),
+            Arguments.of(new NewPerson("First", "")),
+            Arguments.of(new NewPerson("First", null))
+        );
+    }
+
+}
